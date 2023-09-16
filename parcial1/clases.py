503c0eb5e15826706626730d02e8656bfb71d884
shmibblez <watchouthomes@gmail.com>
2023-09-16 15:51:08 -0500

    pitch de bici calculado - intento 1

--- parcial1/clases.py
+++ parcial1/clases.py
@@ -3,7 +3,18 @@ from pyquaternion import Quaternion
 
 
 class Subspace:
-    def __init__(self, x, y, z, ax, ay, az, parentSpace: "Subspace|None"):
+    __contexto__: "Subspace | None" = None
+
+    def __init__(
+        self,
+        x: float,
+        y: float,
+        z: float,
+        ax: float,
+        ay: float,
+        az: float,
+        parentSpace: "Subspace|None",
+    ):
         """
         x, y, z - posicion de origen en parent
         parent - subespacio en el que estamos,
@@ -20,8 +31,19 @@ class Subspace:
         self.ay = ay
         self.az = az
 
+    @staticmethod
+    def definirContexto(contexto):
+        """
+        define subespacio base (afecta los resultados
+        retornados para posiciones absolutas)
+        """
+        Subspace.__contexto__ = contexto
+
     def vecOrigen(self):
-        return [self.x, self.y, self.z]
+        return Subspace.__aplicar__(
+            [self.x, self.y, self.z],
+            Subspace.__contexto__,
+        )
 
     # def xAbs(self):
     #     return (self.parent.xAbs() if self.parent else 0) + self.x
@@ -41,9 +63,44 @@ class Subspace:
     # def zRel(self):
     #     return self.z
 
+    @staticmethod
+    def __aplicar__(punto, sub: "Subspace | None"):
+        """
+        recursivo para comenzar desde los espacios bases
+        punto es lista
+        """
+        if sub == None:
+            return punto
+        aplicado = Subspace.__aplicar__(punto, sub.parent)
+        # restar origen
+        aplicado = np.subtract(aplicado, sub.vecOrigen())
+        # luego rotar
+        xQuat = Quaternion(axis=[1, 0, 0], angle=sub.ax)
+        yQuat = Quaternion(axis=[0, 1, 0], angle=sub.ay)
+        zQuat = Quaternion(axis=[0, 0, 1], angle=sub.az)
+        aplicado = xQuat.rotate(aplicado)
+        aplicado = yQuat.rotate(aplicado)
+        aplicado = zQuat.rotate(aplicado)
+
+        return np.array(aplicado)
+
+    def aplicar(self, punto: list[float]):
+        """
+        lleva vector a este subespacio
+        (aplica sistema de coordenadas)
+        """
+        p = Subspace.__aplicar__(punto, self.parent)
+        return Punto(p[0], p[1], p[2], self)
+
 
 class Punto:
-    def __init__(self, x, y, z, parentSpace: Subspace):
+    def __init__(
+        self,
+        x: float,
+        y: float,
+        z: float,
+        parentSpace: Subspace | None,
+    ):
         self.x = x
         self.y = y
         self.z = z
@@ -51,11 +108,13 @@ class Punto:
 
     def puntoAbs(self):
         """
-        returns start point coordinates and vector in top-most space
+        returns (start point coordinates, vector in top-most space)
+                vector in top-most space is the same as vector in subspace,
+                just in top-most space (diff coordinate sistem)
         """
         start = [0, 0, 0]
         vec = [self.x, self.y, self.z]
-        parentSpace = self.parentSpace
+        parentSpace: Subspace | None = self.parentSpace
         while parentSpace != None:
             xQuat = Quaternion(axis=[1, 0, 0], angle=-parentSpace.ax)
             yQuat = Quaternion(axis=[0, 1, 0], angle=-parentSpace.ay)
@@ -67,6 +126,20 @@ class Punto:
             vec = yQuat.rotate(vec)
             vec = zQuat.rotate(vec)
             start = np.add(start, parentSpace.vecOrigen())
-            parentSpace = self.parentSpace.parent
+            parentSpace = parentSpace.parent
 
-        return (start, vec)
+        start = np.array(start)
+        vec = np.array(vec)
+
+        start = Subspace.__aplicar__(start, Subspace.__contexto__)
+        vec = Subspace.__aplicar__(vec, Subspace.__contexto__)
+        return (
+            Punto(start[0], start[1], start[2], Subspace.__contexto__),
+            Punto(vec[0], vec[1], vec[2], Subspace.__contexto__),
+        )
+
+    def p(self):
+        """
+        retorna componentes como lista sin modificar
+        """
+        return np.array([self.x, self.y, self.z])
--- parcial1/main.py
+++ parcial1/main.py
@@ -1,17 +1,29 @@
 import numpy as np
+import numpy.linalg as lin
 
 from parcial1.clases import Punto, Subspace
 
 # parametros bici (longitud en metros, angulos en grados,
 # tiempo en segundos, velocidad angular en rad/s)
-l = 1  # longitud entre ruedas (+)              (constante)
-r = 0.5  # radio ruedas (+)                     (constante)
-beta = 10  # angulo de tenedor (+)              (constante)
-rho = 0  # angulo de inclinacion / roll         (variable - seleccionar)
-phi = 0  # angulo manuvrio                      (variable - seleccionar)
-psi = 0  # angulo pitch                         (variable - calculado)
-theta = 0  # angulo visto desde arriba          (variable - calculado)
-omega = 10  # velocidad angular rueda trasera   (variable - seleccionar)
+# (constante) longitud entre ruedas
+l = 1  #
+# (constante) radio ruedas
+r = 0.5  # [0.25, 0.75]
+# (constante) angulo de tenedor
+beta = 10  # [0, 45]
+# (variable - seleccionar) angulo de inclinacion / roll
+rho = 0  # [-45, 45]
+# (variable - seleccionar) angulo manuvrio
+phi = 0  # [-40, 40]
+# (variable - calculado) angulo pitch
+psi = 0
+# (variable - calculado) angulo visto desde arriba
+theta = 0
+# (variable - seleccionar) velocidad angular rueda trasera
+omega = 10  # [0.1, 100]
+# (constante) tolerancia para decidir si rueda esta tocando piso
+tolerancia = 0.001  # 0.001 (1mm)
+
 
 # posiciones iniciales de marco b (cambian en el tiempo)
 b_x = 0
@@ -33,8 +45,67 @@ af = Subspace(0, 0, 0, 0, 0, phi, f)
 centro_af = Punto(0, 0, 0, af)
 
 
-def calcularPosicion():
+def ajustarPsi():
     """
-    calcula posicion para que rueda delantera toque el piso
+    ajusta posicion para que rueda delantera toque el piso
+    se corre cada vez que se modifica el manuvrio
     """
-    centro_rueda, _ = centro_af.puntoAbs()
+    Subspace.definirContexto(b)
+    n = 0
+    # correr maximo 10 veces
+    while n < 10:
+        # coordenadas centro de rueda
+        centro_rueda, _ = centro_af.puntoAbs()
+        # un vector unitario en cordenada z negativo (hacia abajo)
+        z_dn = [0, 0, -1]
+        # vectores para definir plano afx-afz en espacio base (v1 y v2)
+        _, v1 = Punto(0, 0, 1, af).puntoAbs()
+        _, v2 = Punto(1, 0, 0, af).puntoAbs()
+        # proyectar z_dn a plano v1-v2. Esta proyeccion se utiliza para encontrar el
+        # punto mas bajo de la rueda (donde tocaria el piso)
+        proy = v1.p() * (np.dot(v1.p(), z_dn)) + v2.p() * (np.dot(v2.p(), z_dn))
+        # normalizar
+        proy = proy / lin.norm(proy)
+        # la linea que toca el piso en la rueda (spoke en rueda de bici)
+        v3 = proy * r
+        # vector de origen a punto que deberia tocar piso (dentro de plano afx-afz)
+        v4 = centro_rueda + v3
+        # si rueda de bici esta dentro de rango, no hay
+        # necesidad de rotar bici, terminar de ajustar
+        if v4[2] <= tolerancia:
+            break
+        # interseccion entre planos abz-abx y el plano formado por by-v4
+        # el eje y del plano ab es ortogonal a abz-abx
+        ort1 = Punto(0, 1, 0, ab).puntoAbs()[1].p()
+        b_y = Punto(0, 1, 0, b).puntoAbs()[1].p()
+        ort2 = np.cross(b_y, v4)
+        # v5 es interseccion de planos
+        v5 = np.cross(ort1, ort2)
+        v5 = v5 / lin.norm(v5)
+        # v5 debe estar en direccion positiva respecto al eje x de b
+        if v5[0] < 0:
+            v5 = v5 * (-1)
+        # v6 -> proyeccion de v4 en v5
+        v6 = ((np.dot(v4, v5)) / lin.norm(v5) ** 2) * v5
+        # encontrar angulo entre v6 y eje x de ab
+        ab_x = Punto(1, 0, 0, ab).puntoAbs()[1].p()
+        angulo_rotacion = np.arccos(
+            np.dot(ab_x, v6) / (lin.norm(ab_x) * lin.norm(v6)),
+        )
+        # coordenada z de v4 determina si toca rotar hacia
+        # arriba o hacia abajo
+        diff = v4[2]
+        # rotar ab por angulo
+        if diff < 0:
+            ab.ay += angulo_rotacion
+        elif diff > 0:
+            ab.ay -= angulo_rotacion
+        n += 1
+
+
+def mover(dx, dy, daz):
+    """
+    la bici se puede trasladar en el plano x-y
+    y puede rotar en el eje z como eje
+    """
+    pass
